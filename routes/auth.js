5d09fee2417cf5ef8a0e439955d921ab446f9094
carlaploscaru <101560739+carlaploscaru@users.noreply.github.com>
2024-06-10 12:40:55 +0300

    production name

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/auth");
-const { body } = require("express-validator/check");
+const { body } = require("express-validator");
 const { User } = require("../models/user");
 const { isEmpty } = require("lodash");
 
